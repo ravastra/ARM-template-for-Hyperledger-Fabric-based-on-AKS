2512a2c4fa815e3e0b1ab760e14100831709beff
shrugupt <56380156+shrugupt@users.noreply.github.com>
2019-12-18 15:17:46 +0530

    Update byn.sh

--- byn.sh
+++ byn.sh
@@ -11,26 +11,27 @@ verifyResult() {
   fi
 }
 
-hlfStatus=$(kubectl get configmap --namespace metadata hlf-status -o jsonpath={.data.hlfStatus})
+hlfStatus=$(kubectl -n metadata get configmap hlf-status -o jsonpath={.data.hlfStatus})
 if [ ! "$hlfStatus" == "Done" ]; then
   res=1
   verifyResult $res "HLF Organization is not setup yet. Can't run the script."
 fi
-kubectl apply -f ./fabric-admin.yaml
+adminNamespace="hlf-admin"
+kubectl -n ${adminNamespace} apply -f ./fabric-admin.yaml
 theargs=""
 for i in "$@" ; do
    theargs="${theargs} \"$i\""
 done
-kubectl wait --for=condition=Ready pod -l name=fabric-admin --namespace hlf-admin --timeout=300s &> $LOG_FILE
+kubectl -n ${adminNamespace} wait --for=condition=Ready pod -l name=fabric-admin --timeout=300s &> $LOG_FILE
 res=$?
 verifyResult $res "Failed to start fabric-admin pod"
 echo
 echo "======== Started fabric-admin pod!!! =========="
 echo
 
-FABRIC_ADMIN_POD=$(kubectl get pods -l name=fabric-admin --namespace hlf-admin -ojsonpath={.items[0].metadata.name})
-kubectl exec ${FABRIC_ADMIN_POD} --namespace hlf-admin -- bash -c "/var/hyperledger/consortiumScripts/byn2.sh ${theargs}"
-kubectl delete pod ${FABRIC_ADMIN_POD} --namespace hlf-admin &> $LOG_FILE
+FABRIC_ADMIN_POD=$(kubectl -n ${adminNamespace} get pods -l name=fabric-admin -ojsonpath={.items[0].metadata.name})
+kubectl -n ${adminNamespace} exec ${FABRIC_ADMIN_POD} -- bash -c "/var/hyperledger/consortiumScripts/byn2.sh ${theargs}"
+kubectl -n ${adminNamespace} delete pod ${FABRIC_ADMIN_POD} &> $LOG_FILE
 res=$?
 verifyResult $res "Deletion of fabric-admin pod Failed"
 echo
