3ddf4e8f209d4fce59611f8fd49e5eb2254f4e21
Shruti Gupta <shrugupt@microsoft.com>
2019-12-18 23:52:52 +0530

    Revert "Merge branch 'shr-namespace' into v1.0.0"
    This reverts commit 6a6d3c110f9c7f6f19f3ff491f9bfbf3a02143a9, reversing
    changes made to ee8e1cdb838a8b5bee86a6ce7bed9758be7dc508.

--- byn.sh
+++ byn.sh
@@ -11,27 +11,26 @@ verifyResult() {
   fi
 }
 
-hlfStatus=$(kubectl -n metadata get configmap hlf-status -o jsonpath={.data.hlfStatus})
+hlfStatus=$(kubectl get configmap hlf-status -o jsonpath={.data.hlfStatus})
 if [ ! "$hlfStatus" == "Done" ]; then
   res=1
   verifyResult $res "HLF Organization is not setup yet. Can't run the script."
 fi
-adminNamespace="hlf-admin"
-kubectl -n ${adminNamespace} apply -f ./fabric-admin.yaml
+kubectl apply -f ./fabric-admin.yaml
 theargs=""
 for i in "$@" ; do
    theargs="${theargs} \"$i\""
 done
-kubectl -n ${adminNamespace} wait --for=condition=Ready pod -l name=fabric-admin --timeout=300s &> $LOG_FILE
+kubectl wait --for=condition=Ready pod -l name=fabric-admin --timeout=300s &> $LOG_FILE
 res=$?
 verifyResult $res "Failed to start fabric-admin pod"
 echo
 echo "======== Started fabric-admin pod!!! =========="
 echo
 
-FABRIC_ADMIN_POD=$(kubectl -n ${adminNamespace} get pods -l name=fabric-admin -ojsonpath={.items[0].metadata.name})
-kubectl -n ${adminNamespace} exec ${FABRIC_ADMIN_POD} -- bash -c "/var/hyperledger/consortiumScripts/byn2.sh ${theargs}"
-kubectl -n ${adminNamespace} delete pod ${FABRIC_ADMIN_POD} &> $LOG_FILE
+FABRIC_ADMIN_POD=$(kubectl get pods -l name=fabric-admin -ojsonpath={.items[0].metadata.name})
+kubectl exec ${FABRIC_ADMIN_POD} -- bash -c "/var/hyperledger/consortiumScripts/byn2.sh ${theargs}"
+kubectl delete pod ${FABRIC_ADMIN_POD} &> $LOG_FILE
 res=$?
 verifyResult $res "Deletion of fabric-admin pod Failed"
 echo
